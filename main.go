dfc061e5f7d92c2bf14f17e10ceb68c458bb9364
kostaskoukos <kkonst2014@gmail.com>
2024-04-13 16:42:30 +0300

    bot becoming online!

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func iferr(err *error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
 func main() {
+	godotenv.Load(".env")
+	KEY := os.Getenv("BOT_KEY")
+	session, err := discordgo.New("Bot " + KEY)
+	iferr(&err)
+	{
+		err := session.Open()
+		iferr(&err)
+	}
 	fmt.Println("hey")
 }
