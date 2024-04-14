b946b60234f2ec475e50988ec54ae97a6c941a1c
kostaskoukos <kkonst2014@gmail.com>
2024-04-14 17:34:40 +0300

    first slash command working & some cleanup

--- main.go
+++ main.go
@@ -15,37 +15,56 @@ func iferr(err error) {
 	}
 }
 
-func main() {
+var (
+	TOKEN   string
+	APP_KEY string
+	session *discordgo.Session
+)
+
+func init() {
 	log.Println("loading env vars...")
 	godotenv.Load(".env")
-	BOT_KEY := os.Getenv("BOT_KEY")
-	APP_KEY := os.Getenv("APP_KEY")
-	log.Println("creating bot...")
-	session, err := discordgo.New("Bot " + BOT_KEY)
-	iferr(err)
-	{
-		cmds, err := session.ApplicationCommandBulkOverwrite(APP_KEY, "", []*discordgo.ApplicationCommand{
-			{
-				Name:        "test",
-				Description: "Showcase of a basic slash command",
-			},
-		})
+	TOKEN = os.Getenv("TOKEN")
+	APP_KEY = os.Getenv("APP_KEY")
 
-		session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			log.Println("interaction with: ", i.Member)
-		})
-		log.Println("cmds: ", cmds)
-		iferr(err)
+	log.Println("creating bot...")
+
+	var err error
+	session, err = discordgo.New("Bot " + TOKEN)
+	if err != nil {
+		log.Fatalf("Cannot create the bot: %v", err)
+	}
+}
+
+func main() {
+	_, err := session.ApplicationCommandBulkOverwrite(APP_KEY, "", []*discordgo.ApplicationCommand{
+		{
+			Name:        "test",
+			Description: "Showcase of a basic slash command",
+		},
+	})
+	if err != nil {
+		log.Fatalf("Couldn't register commands: %v", err)
 	}
 	{
+		session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Hey there! Congratulations, you just executed your first slash command",
+				},
+			})
+		})
+
 		session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
 			log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
 		})
 
-		err := session.Open()
-		iferr(err)
 		log.Println("starting websocket connection...")
-
+		err := session.Open()
+		if err != nil {
+			log.Fatalf("Cannot open websocker session: %v", err)
+		}
 		defer session.Close()
 
 		stop := make(chan os.Signal, 1)
